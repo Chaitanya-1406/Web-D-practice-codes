1f6b6f462ecf106840dcb1d493dcf49f34083ec8
Chaitanya-1406 <137593141+Chaitanya-1406@users.noreply.github.com>
2023-07-01 13:51:26 +0530

    Add files via upload

---
+++ Working.js
@@ -0,0 +1,74 @@
+const upPlayer1 =  document.querySelector('#upPlayer1');
+const upPlayer2 =  document.querySelector('#upPlayer2');
+const p1Display = document.querySelector('p1Display');
+const p2Display = document.querySelector('p2Display');
+
+    let p1score = 0;
+    let p2score = 0;
+
+upPlayer1.addEventListener('click', function()
+{
+   alert("CLicked me")
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('button').addEventListner('Click', function(evt)
+// {
+//     console.log(evt)
+// })
+
+// // const input = document.querySelector('input');
+// // input.addEventListener('KEYDOWN', function( )
+// // {
+// //    console.log("KEYDOWN")
+// // nc
+
+// window.addEventListner('Keydown', function(e)){
+
+// switch(e.code){
+
+//     case'ArrowUP':
+//         console.log("UP!");
+//         break;
+//     case'ArrowDown':
+//         console.log("Down!");
+//         break;
+//     default:
+//         console.log("IGNORED!");        
+
+// }
+// }
